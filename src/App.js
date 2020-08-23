4560fe01f8c7bbdaae193abededbaba126b330a6
Mihail Iftodi <mihail@spectrocloud.com>
2020-08-23 11:39:46 +0300

    some code for prompt to install

--- src/App.js
+++ src/App.js
@@ -2,7 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let deferredPrompt = window;
+
+function installToHomeScreen(e) {
+  localStorage.setItem("shouldShowInstall", false);
+   // Show the install prompt
+   deferredPrompt.prompt("install?");
+   // Wait for the user to respond to the prompt
+   deferredPrompt.userChoice.then((choiceResult) => {
+     if (choiceResult.outcome === 'accepted') {
+       console.log('User accepted the install prompt');
+     } else {
+       console.log('User dismissed the install prompt');
+     }
+   });
+}
+
+window.addEventListener('appinstalled', (evt) => {
+  // Log install to analytics
+  console.log('INSTALL: Success');
+});
+
 function App() {
+  window.addEventListener('beforeinstallprompt', (e) => {
+    console.log(e, "intra");
+    // Prevent the mini-infobar from appearing on mobile
+    e.preventDefault();
+    // Stash the event so it can be triggered later.
+    deferredPrompt = e;
+
+    localStorage.setItem("shouldShowInstall", true);
+  });
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,6 +49,7 @@ function App() {
         >
           Learn React
         </a>
+      <button onClick={installToHomeScreen}>Add to home screen</button>
       </header>
     </div>
   );
